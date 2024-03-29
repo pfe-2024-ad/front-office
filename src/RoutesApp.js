40f542a6cd9298c74f85127b170afaf8b44d7ce8
SanaaBou <boualilasanaa1@gmail.com>
2024-03-29 13:30:40 +0000

    s3/46_soumettre_cin_recto_verso_et_selfie

--- src/RoutesApp.js
+++ src/RoutesApp.js
@@ -3,10 +3,21 @@ import {Route, Routes} from 'react-router-dom'
 import Home from './components/home/Home.js'
 
 
+
+
+import OcrStep from './components/commun/steps/ocr-step/OcrStep.js'
+
+
 function RoutesApp() {
     return(
         <Routes>
             <Route path="/" element={<Home />} />
+
+
+
+
+
+            <Route path="/verification-identite" element={<OcrStep />} />
         </Routes>
     )
 }
---
+++ src/components/commun/steps/ocr-step/cin-selfie-check/CinSelfieCheck.js
@@ -0,0 +1,66 @@
+import CinCheck from "./cin/cin-check/CinCheck.js";
+import SelfieCheck from "../cin-selfie-check/selfie/selfie-check/SelfieCheck.js";
+import './CinSelfieCheck.css';
+import cinlogo from "../../../../assets/cin-check.png";
+import selfielogo from "../../../../assets/selfie-check.png";
+import Button from 'react-bootstrap/Button';
+
+import { useState, useEffect } from 'react';
+import { useNavigate } from "react-router-dom";
+
+import { useGlobalState } from '../../GlobalState.js';
+
+function CinSelfieCheck() {
+
+    const { setResponseGlobal } = useGlobalState();
+    const { file1, file2, file3 } = useGlobalState();
+
+    let idClient = sessionStorage.getItem('idClient');
+
+    
+    const [response, setResponse] = useState({similarity: null, nom: null,  prenom: null, dateNaissance: null, cin: null, adresseResidence: null});
+
+    let navigate = useNavigate();
+
+    
+
+    useEffect( () => {
+        if(response.similarity === "100%"){
+            setResponseGlobal(response);
+            navigate("/verification-donnees");
+        }
+    })
+
+    function handleUpload(e) {
+        e.preventDefault();
+        const formData = new FormData();
+        formData.append('file1', file1);
+        formData.append('file2', file2);
+        formData.append('file3', file3);
+        formData.append('id', idClient);
+        
+        fetch('http://localhost:7777/agd/client-service/api/get-cin-infos', {
+            method: 'POST',
+            body: formData
+        })                
+        .then(response => response.json())
+        .then(data => setResponse({similarity: data.similarity, nom: data.nom,  prenom: data.prenom, dateNaissance: data.dateNaissance, cin: data.cin, adresseResidence: data.adresseResidence}));
+             
+    }
+
+    return (
+        <div className="style-cin-selfie-check">
+            <p className="p-check">
+                Identifiez-vous grâce au scan de votre CIN et à l'authentification biométrique
+            </p>
+            
+            <CinCheck checklogo={cinlogo} descriptif={"Charger votre piéce d'identité (CIN/Carte de séjour)"}/>
+            <SelfieCheck checklogo={selfielogo} descriptif={"Vérifier votre identité par un selfie check"}/>
+            
+            <Button className='button-upload' style={{marginBottom: "4%"}} type='submit' onClick={handleUpload}>Suivant</Button>
+
+        </div>
+    );
+
+}
+export default CinSelfieCheck
