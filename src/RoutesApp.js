e35abf59fd5eddca3275158bda3dc3bd8591b3f7
SanaaBou <boualilasanaa1@gmail.com>
2024-03-29 14:12:24 +0000

    s3/47_vérifier_et_compléter_données_personnelles

--- src/RoutesApp.js
+++ src/RoutesApp.js
@@ -2,11 +2,16 @@ import {Route, Routes} from 'react-router-dom'
 
 import Home from './components/home/Home.js'
 
+import ConfirmationStep from './components/commun/steps/confirmation-step/ConfirmationStep.js'
+
 
 function RoutesApp() {
     return(
         <Routes>
             <Route path="/" element={<Home />} />
+
+            <Route path="/verification-donnees" element={<ConfirmationStep />} />
+
         </Routes>
     )
 }
